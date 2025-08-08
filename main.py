c18b4a1a0e031c61ba97bc7adf21d23c09d6a642
vincener <542948030@qq.com>
2025-08-08 14:17:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,12 +28,12 @@ def push(content):
 
 # 会不定时更新域名，记得Sync fork
 
-login_url = 'https://ikuuu.ch/auth/login'
-check_url = 'https://ikuuu.ch/user/checkin'
-info_url = 'https://ikuuu.ch/user/profile'
+login_url = 'https://ikuuu.de/auth/login'
+check_url = 'https://ikuuu.de/user/checkin'
+info_url = 'https://ikuuu.de/user/profile'
 
 header = {
-        'origin': 'https://ikuuu.ch',
+        'origin': 'https://ikuuu.de',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
 
